ea73c13406edec3e726fb18849a44768476a1fd1
Lidia <lida121290@mail.ru>
2022-11-27 20:38:14 +0600

    Домашнее задание 2, Task1_3 и Task1_4

---
+++ 2_lesson/Homework2/Task1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3:Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Задайте число: ");
+
+int number = int.Parse(Console.ReadLine());
+string num2 = number.ToString();
+
+if (num2.Length >= 3)
+{
+   Console.WriteLine(num2[2]); 
+}
+else
+{
+   Console.WriteLine("Третьей цифры нет."); 
+}
