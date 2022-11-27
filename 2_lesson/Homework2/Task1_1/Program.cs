e556d242df3897adf09cd5724e07288e4ec9c4ca
Lidia <lida121290@mail.ru>
2022-11-27 19:15:56 +0600

    Домашнее задание 2, Task1_1 и Task1_2

---
+++ 2_lesson/Homework2/Task1_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 1:      Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите техзначное число:");
+int number = int.Parse(Console.ReadLine());
+string num2 = number.ToString();
+Console.WriteLine(num2[1]);
+
---
+++ 2_lesson/Homework2/Task1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2:Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 999);
+
+System.Console.WriteLine(number);
+
+string number_to_str = number.ToString();
+
+string new_number = number_to_str.Remove(number_to_str.Length -2, 1);
+
+System.Console.WriteLine(new_number);
