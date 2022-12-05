96291846746863bcf861bcc60276c442f5bc8138
Lidia <lida121290@mail.ru>
2022-12-05 23:49:34 +0600

    5_lesson 5_0

--- 4_lesson/Homework4/Task4_3/Program.cs
+++ 4_lesson/Homework4/Task4_3/Program.cs
@@ -9,3 +9,27 @@ for(int i = 0; i < 8; i++)
 }    
 string str = string.Join(" ", array);
 Console.WriteLine($"[{str}]");
+
+/*void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] EightMass(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1, 100);
+    return arr;
+}
+
+int[] arr_1 = EightMass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+int[] arr_2 = EightMass(int.Parse(Console.ReadLine()));
+Print(arr_2);*/
+
