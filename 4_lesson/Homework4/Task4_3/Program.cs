f4ed9727f26bfb25be6582c00f3a079e1b86107d
Lidia <lida121290@mail.ru>
2022-12-05 19:14:16 +0600

    Homework4 Task4_2

---
+++ 4_lesson/Homework4/Task4_3/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string num1 = number.ToString();
+int result = 0;
+
+for (int i = 0; i < num1.Length; i++)
+{
+    result = result + num1[i];
+    System.Console.WriteLine(result);
+}
+
+System.Console.WriteLine(result);
