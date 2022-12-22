622923cb7386ea6a76fa9cf3f3c84941d7fdd98f
Lidia <lida121290@mail.ru>
2022-12-22 20:01:52 +0600

    9_lesson Homework

---
+++ 9_lesson/Homework9/9_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Sum(int n, int m)
+{
+    if(n > m)
+        return 0;
+    return n + Sum(n+1, m);
+}
+System.Console.WriteLine(Sum(1,15));
